d8ac8ec2c065d9b01322a83da2ab103299b2cbaf
Sai Sunny Kothuri <159966453+saisunny07@users.noreply.github.com>
2024-07-08 17:50:51 -0500

    Delete code directory

--- code/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull saisunny05/hello-world
-
-# Run the Docker image as a container
-docker run -d -p 3000:3000 saisunny05/hello-world
--- code/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-containerId = 'docker ps | awk -F " " '{print$1}''
-docker rmi -f $containerId

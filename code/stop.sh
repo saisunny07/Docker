79d5dcea8d9bb0d12deea682cc7abf1be8d72712
Sai Sunny Kothuri <159966453+saisunny07@users.noreply.github.com>
2024-07-08 13:36:51 -0500

    update folders

--- code/stop.sh
+++ code/stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 containerId = 'docker ps | awk -F " " '{print$1}''
 docker rm -f $containerId
--- scripts/start_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker pull saisunny07/hello-world
-docker run -p3000:3000 saisunny07/hello-world

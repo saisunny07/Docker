bed6f393f70964262dadd2e80ccb9df4694bf1c9
new-at-654654561453 <kothurisaisunny5@gmail.com>
2024-07-08 20:35:08 -0500

    updated files

---
+++ scripts/start_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker pull saisunny07/hello-world
+
+docker run -p3000:3000 saisunny07/hello-world
---
+++ scripts/stop_container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+containerId = 'docker ps | awk -F " " "print$1"'
+
+docker rm -f $containerId
